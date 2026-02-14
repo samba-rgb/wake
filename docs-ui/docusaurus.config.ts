512ad89a423ad25b140004ae6f2255a16093c888
samba-rgb <samba24052001@gmail.com>
2026-02-15 03:27:58 +0530

    Fix baseUrl configuration for GitHub Pages deployment
    - Change baseUrl from '/wake/' to '/' as recommended by Docusaurus
    - This should resolve the site loading issues with GitHub Actions deployment

--- docs-ui/docusaurus.config.ts
+++ docs-ui/docusaurus.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://samba-rgb.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wake/',
+  // For GitHub Actions deployment, it should be '/' 
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
